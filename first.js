5e78a58e87b8cc8702b5689bf5ec5dace798fef6
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-02-27 17:01:33 +0530

    JS EXplore

---
+++ first.js
@@ -0,0 +1,13 @@
+const student = {
+  fullName: "Shudhanshu Arya",
+  age: 20,
+  cgpa: 8.9,
+  isPass: true,
+};
+
+
+//if you want to re-declared value you can write like this
+student["fullName"] = "Shudhanhshu Sekhar";
+
+// console.log(student["cgpa"]);
+console.log(student.fullName);
