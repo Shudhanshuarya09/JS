abec2e21f3b6bf92b6783988837738186fe6f297
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-02-28 19:01:40 +0530

    all type of loops done

---
+++ loops.js
@@ -0,0 +1,33 @@
+//for loop
+
+for(let i = 1; i<= 50; i++)
+{
+  console.log("Hello");
+
+}
+
+//example
+ let sum = 0;
+ for(let i =1; i<=5; i++)
+ {
+  sum = sum + i;
+ }
+
+ console.log("sum =", sum)
+
+
+
+ //while loop
+ let i =1;
+ while (i<=3)
+{
+  console.log("hello");
+  i++;
+}
+
+//do-while
+let i = 1;
+do {
+  console.log("hello");
+  i++;
+}while(i <= 8);
