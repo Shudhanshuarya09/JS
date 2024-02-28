ec2e247f2e531619cba5c6253a1ba112a5753da4
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-02-28 17:32:58 +0530

    Question 2

---
+++ question2.js
@@ -0,0 +1,12 @@
+const instaBio = {
+  fullName : "Shudhanshu Arya",
+  isFollow : true,
+  follower : 200 + "K",
+  following : 50,
+  bio : "Sher kar duniya ki gafeel , zindgani phir kha "
+};
+
+
+
+console.log(instaBio);
+console.log(typeof ["fullName"]);
