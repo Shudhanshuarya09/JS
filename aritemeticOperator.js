8e6293117765faefde39b0130e15598aba9a17f7
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-02-28 17:42:13 +0530

    Arithemetic operator

---
+++ aritemeticOperator.js
@@ -0,0 +1,15 @@
+let a = 14;
+let b = 8;
+
+c = a + b;
+
+
+console.log("a = 14 & b = 8")
+console.log("a + b =", a+b);
+console.log("a - b =", a-b);
+console.log("a * b =", a*b);
+console.log("a / b =", a/b);
+
+
+//we can also do that
+console.log(c)
