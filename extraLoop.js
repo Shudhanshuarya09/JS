7a60f0c11346b2b9a4109864c3bec26d88870af9
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-02-29 16:23:16 +0530

    for-in-loop

--- extraLoop.js
+++ extraLoop.js
@@ -1,12 +1,27 @@
 //for-of-loop
 
-let word = "SANT LONGOWAL INSTITUTE OF ENGINEERING AND TECHNOLOGY";
+// let word = "SANT LONGOWAL INSTITUTE OF ENGINEERING AND TECHNOLOGY";
 
-let size = 0;
-for (let i of word)
+// let size = 0;
+// for (let i of word)
+// {
+//   console.log("i =", i);
+//   size++;
+// }
+
+// console.log("String size = ", size)
+
+
+// for-in-loop
+
+let student = {
+  name : "Shudhanshu Arya",
+  age : 21,
+  cgpa : 8.9,
+  isPass : true,
+};
+
+for (let key in student)
 {
-  console.log("i =", i);
-  size++;
-}
-
-f
+  console.log("key = ", key , "values = ", student[key])
+};
