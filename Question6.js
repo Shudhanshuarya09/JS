22efcf632e9bee4f6a3d919fc210386ec03ac5e9
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-02-29 23:49:37 +0530

    user name question solve

--- Question6.js
+++ Question6.js
@@ -1,4 +1,5 @@
 let name = prompt("Enter your name");
 
-let newName = "@"+name+Str.length(name);
-console.log(newname);
+let generalizeName = name.toLowerCase();
+let newName = `@${generalizeName}${generalizeName.length}`;
+console.log(newName);
--- string.js
+++ string.js
@@ -1,8 +1,8 @@
 
-// let str = "Amritsar";
-// console.log(str[4]);
+// // let str = "Amritsar";
+// // console.log(str[4]);
 
-//Template literals and string interolation concept
+// //Template literals and string interolation concept
 
 let student = 
 {
@@ -11,7 +11,22 @@ let student =
   isPass : true,
   cgpa : 9
 };
-//console.log("grade of", student.name, "is", student.cgpa, "so", student.isPass);   //-->this is aam zindgi
+console.log("grade of", student.name, "is", student.cgpa, "so", student.isPass);   //-->this is aam zindgi
 
 let New = `the grade of ${student.name} is ${student.cgpa} is ${student.isPass};`   //-->this is coder zindgi
 console.log(New);
+
+
+// upperCase - lowerCase concept
+let Name = "Shudhanshu Arya";
+console.log(name);
+let newName = Name.toUpperCase();
+console.log(newName); 
+
+
+//concat --> to join two string
+let name1 = "Shudhasnhu";
+let name2 = "Arya";
+
+let join = name1.concat(name2);
+console.log(join);
