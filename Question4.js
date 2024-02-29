de27ebb70e61c08456537f4905efeb33a8606195
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-02-29 15:52:48 +0530

    Question 4 done

---
+++ Question4.js
@@ -0,0 +1,22 @@
+let marks = prompt("Enter your marks");
+
+if (marks >= 90)
+{
+  console.log("Grade of student is A");
+}
+else if (marks >= 70)
+{
+  console.log("Grade of student is B");
+}
+else if (marks >= 60)
+{
+  console.log("Grade of student is C");
+}
+else if (marks >= 50)
+{
+  console.log("Grade of student is D");
+}
+else 
+{
+  console.log("Grade of student is Fail");
+}
