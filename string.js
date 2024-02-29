1faa54d5a2b6252e7875c2fc41f21189c8f7afa1
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-02-29 23:24:11 +0530

    Strings concept

--- string.js
+++ string.js
@@ -1,1 +1,17 @@
+
+// let str = "Amritsar";
+// console.log(str[4]);
+
 //Template literals and string interolation concept
+
+let student = 
+{
+  name : "Shudhasnhu Arya",
+  age : 45,
+  isPass : true,
+  cgpa : 9
+};
+//console.log("grade of", student.name, "is", student.cgpa, "so", student.isPass);   //-->this is aam zindgi
+
+let New = `the grade of ${student.name} is ${student.cgpa} is ${student.isPass};`   //-->this is coder zindgi
+console.log(New);
