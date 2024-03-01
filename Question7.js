4b355917eb2b81fca831f5c43bc05dbe5ea95e92
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-03-01 17:15:49 +0530

    question on array concept

--- Question7.js
+++ Question7.js
@@ -1,1 +1,13 @@
 //Array ka questions
+//Question --> For a given array with marks of student [85,97,44,37,76,60].Find the average marks of the entire class.
+
+let marks = [85,97,44,37,76,60];
+
+let sum = 0;
+
+for (let val of marks)
+{
+  sum += val;
+}
+let avg = sum/marks.length;
+console.log(`Average marks of the class = ${avg}`);
