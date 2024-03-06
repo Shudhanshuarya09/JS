6691fb54412ea50e8e2b7f49d0a3e765281961b6
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-03-06 22:21:25 +0530

    more js added

--- ticTacToe.js
+++ ticTacToe.js
@@ -33,5 +33,15 @@ boxes.forEach((box) => {
 
 const checkWinner = () =>{
 
-  
-}
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1val != '' && pos2Val != "" && pos3Val != "") {
+      if(pos1Val === pos2Val && pos2Val === pos3Val) {
+
+      }
+    }
+  }
+};
