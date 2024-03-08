6c7ad150eab40cea5d0872d9930ffb883333f45d
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-03-08 17:02:55 +0530

    Promise chain concept

--- Async-Await.js
+++ Async-Await.js
@@ -24,3 +24,20 @@ async function getAllData() {
   await getData(6);
   
 }
+
+
+//Promise Chain
+console.log("getting data1 ...");
+getData(1)
+.then((res) => {
+  console.log("getting data2 ...");
+  return getData(2);
+})
+.then((res) => {
+  console.log("getting data3 ...");
+  return getData(3);
+})
+.then((res) => {
+  console.log(res);
+ 
+});
